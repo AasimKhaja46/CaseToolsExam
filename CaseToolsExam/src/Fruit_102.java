777219c3ab665e05b57a516e85183599a4365752
Aasim khaja <Aasim khaja@LAPTOP-B3NHQS6B>
2021-07-23 14:31:11 +0530

    CaseToolExam.commit

---
+++ CaseToolsExam/src/Fruit_102.java
@@ -0,0 +1,19 @@
+
+public class Fruit_102 {
+
+	
+	String color;
+	
+	void Fruit(String color)
+	{
+		this.color=color;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
